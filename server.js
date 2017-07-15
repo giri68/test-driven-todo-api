24e965e73d097a997aa2d59574a30871f4196d47
giri68 <giri.pdt@gmail.com>
2017-07-15 00:47:15 -0700

    my own api created and tested in postman

--- server.js
+++ server.js
@@ -1,124 +1,56 @@
-// require express and other modules
-var express = require('express'),
-    app = express(),
-    bodyParser = require('body-parser');
-
-// configure bodyParser (for receiving form data)
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// serve static files from public folder
-app.use(express.static(__dirname + '/public'));
-
-/************
- * DATABASE *
- ************/
-
-// our database is an array for now with some hardcoded values
-var todos = [
-  { _id: 7, task: 'Laundry', description: 'Wash clothes' },
-  { _id: 27, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
-  { _id: 44, task: 'Homework', description: 'Make this app super awesome!' }
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+var customers = [
+  {id: 1, name: "John", address: "San Franscisco", phone: "11111"},
+  {id: 2, name: "James", address: "Oakland", phone: "22222"},
+  {id: 3, name: "Kevin", address: "Richmond", phone: "33333"},
+  {id: 4, name: "Sepp", address: "Albany", phone: "44444"}
 ];
 
-/**********
- * ROUTES *
- **********/
+app.get("/api/customers", function(req, res){
+  res.json(customers);
+});
+app.get("/api/customers/:id", function(req, res){
+  var requiredId = parseInt(req.params.id);
+  var requiredData = customers.filter(function(data){
+    return requiredId === data.id;
+  })[0];
+  res.json(requiredData);
+});
+app.post("/api/customers", function(req,res){
+  var newData = req.body;
+  if (customers.length > 0){
+    newData.id = customers[customers.length -1].id + 1;
+  } else {
+    newData.id = 1;
+  }
+  customers.push(newData);
+  res.json(newData);
+});
+app.put("/api/customers/:id", function(req, res){
+  var requiredId = parseInt(req.params.id);
+  var updateData = req.body;
+  var requiredData = customers.filter(function(data){
+    return requiredId === data.id;
+  })[0];
+  requiredData.name = updateData.name;
+  requiredData.address = updateData.address;
+  requiredData.phone = updateData.phone;
 
-/*
- * HTML Endpoints
- */
-
-app.get('/', function homepage(req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.json(requiredData);
 });
 
-
-/*
- * JSON API Endpoints
- *
- * The comments below give you an idea of the expected functionality
- * that you need to build. These are basic descriptions, for more
- * specifications, see the todosTest.js file and the outputs of running
- * the tests to see the exact details. BUILD THE FUNCTIONALITY IN THE
- * ORDER THAT THE TESTS DICTATE.
- */
-
-app.get('/api/todos/search', function search(req, res) {
-  /* This endpoint responds with the search results from the
-   * query in the request. COMPLETE THIS ENDPOINT LAST.
-   */
-var searchTerm  = req.query.q;
-var filtered = todos.filter(function(todo){
-  return (todo.task.toLowerCase().includes(searchTerm.toLowerCase()) || todo.description.toLowerCase().includes(searchTerm.toLowerCase()));
-
+app.delete("/api/customers/:id", function(req, res){
+  var requiredId = parseInt(req.params.id);
+  var requiredData = customers.filter(function(data){
+    return requiredId === data.id;
+  })[0];
+  customers.splice(customers.indexOf(requiredData),1);
+res.json(requiredData);
 });
-res.json({data: filtered});
-});
-
-app.get('/api/todos', function index(req, res) {
-  /* This endpoint responds with all of the todos
-   */
-   res.json({data:todos});
-});
-
-app.post('/api/todos', function create(req, res) {
-  /* This endpoint will add a todo to our "database"
-   * and respond with the newly created todo.
-   */
-var newToDo = req.body;
-if (todos.length > 0){
-  newToDo._id = todos[todos.length-1]._id + 1;
-} else {
-  newToDo._id = 1;
-}
-todos.push(newToDo);
-res.json(newToDo);
-});
-
-app.get('/api/todos/:id', function show(req, res) {
-  /* This endpoint will return a single todo with the
-   * id specified in the route parameter (:id)
-   */
-   var toDoId = parseInt(req.params.id);
-   var foundToDo = todos.filter(function(toDo){
-
-     return toDo._id ==toDoId;
-   })[0];
-   res.json(foundToDo);
-});
-
-app.put('/api/todos/:id', function update(req, res) {
-  /* This endpoint will update a single todo with the
-   * id specified in the route parameter (:id) and respond
-   * with the newly updated todo.
-   */
-   var toDoId = parseInt(req.params.id);
-   var toDoUpdate = todos.filter(function(todo){
-     return todo._id === toDoId;
-   })[0];
-   toDoUpdate.task = req.body.task;
-   toDoUpdate.description = req.body.description;
-   res.json(toDoUpdate);
-});
-
-app.delete('/api/todos/:id', function destroy(req, res) {
-  /* This endpoint will delete a single todo with the
-   * id specified in the route parameter (:id) and respond
-   * with success.
-   */
-   var toDoId = parseInt(req.params.id);
-   var toDoDelete = todos.filter(function (toDo){
-     return toDo._id === toDoId;
-   })[0];
-   todos.splice(todos.indexOf(toDoDelete), 1);
-   res.json(toDoDelete);
-});
-
-/**********
- * SERVER *
- **********/
-
-// listen on port 3000
-app.listen(3000, function() {
-  console.log('Server running on http://localhost:3000');
-});
+app.listen(3000, function(){
+  console.log("HTTP server is listening at local port 3000");
+})
